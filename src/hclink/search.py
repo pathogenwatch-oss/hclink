d63a5bb29ef3e54c7946feec4e858be90fde048f
Corin Yeats <corin.yeats@cgps.group>
2025-08-27 15:00:25 +0100

    Code cleaning.

--- src/hclink/search.py
+++ src/hclink/search.py
@@ -1,38 +1,27 @@
-import math
-
 import numpy as np
 from numba import carray, cfunc, njit, types
 from usearch.compiled import MetricKind, MetricSignature
 from usearch.index import CompiledMetric, Match, Matches
-
-signature = types.float32(
-    types.CPointer(types.float32),
-    types.CPointer(types.float32),
-    types.uint64)
-
-
-@njit
-def debug_print(*args):
-    print(*args)
-
 
 def get_compiled_metric(max_gaps: int):
     return CompiledMetric(pointer=get_distance_score(max_gaps).address, kind=MetricKind.Unknown, signature=MetricSignature.ArrayArraySize)
 
 
 def get_distance_score(max_gaps: int):
-
-    @cfunc(signature)
+    @cfunc(types.float32(
+        types.CPointer(types.float32),
+        types.CPointer(types.float32),
+        types.uint64))
     @njit
     def hiercc_distance(a_ptr, b_ptr, ndim):
         a_array = carray(a_ptr, ndim)
         b_array = carray(b_ptr, ndim)
-        
+
         gaps_a = 0
         gaps_b = 0
         gaps_both = 0
         distance = 0
-        
+
         for i in range(ndim):
             a_val = a_array[i]
             b_val = b_array[i]
@@ -42,17 +31,20 @@ def get_distance_score(max_gaps: int):
                 distance += (a_val != 0 and b_val != 0)
                 gaps_a += (a_val == 0)
                 gaps_b += (b_val == 0)
-        
+
+        # Set the maximum distance if there are too many gaps
         if gaps_both >= max_gaps or distance >= ndim:
             return np.float32(ndim)
-        
+
+        # Set the minimum distance if the distance is zero and all profiles have zero gaps
         if distance == 0 and gaps_a == 0 and gaps_b == 0:
             return np.float32(0.0)
-        
+
+        # Calculate the HIERCC distance
         ndim_f32 = np.float32(ndim)
         query_core = ndim_f32 - np.float32(gaps_a + gaps_both) - np.float32(0.03 * ndim)
         common_core = ndim_f32 - np.float32(gaps_a + gaps_b + gaps_both)
-        
+
         if common_core >= query_core:
             if common_core == 0:
                 return ndim_f32
@@ -61,10 +53,11 @@ def get_distance_score(max_gaps: int):
             if query_core == 0:
                 return ndim_f32
             cc_distance = ((ndim_f32 * np.float32(distance + query_core - common_core)) / query_core) + np.float32(0.5)
-        
+
         return min(cc_distance, ndim_f32)
 
     return hiercc_distance
+
 
 def infer_hiercc_code(hier_cc_distance: float,
                       hiercc_thresholds: list[int],
